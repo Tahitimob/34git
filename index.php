863752efed7dd98bc2f194f6fd2175e1b2a7b40d
Tahitimob <toast.123@laposte.net>
2016-06-30 11:35:36 +0200

    écriture dans un fichier

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 $file = fopen("texte.txt", "r");
 var_dump(fread($file, filesize("texte.txt")));
 fclose($file);
+
+
+//^pour écrire dans un fichier
+
+$file = fopen("texte.log", "w+");
+fwrite($file, "mon contenu");
+fclose($file);
+
+
 ?>
